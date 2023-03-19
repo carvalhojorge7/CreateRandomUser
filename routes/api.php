cef7f76e7e30c43b464f400747ca2c4e54fd9c37
Jorge Carvalho <jorge@softbuilder.com.br>
2023-03-19 03:53:40 -0300

    update laravel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\ApiController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,11 +8,10 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
 Route::get('json_generate', [ApiController::class, 'generate']);
 Route::post('generate', [ApiController::class, 'generate']);
-
